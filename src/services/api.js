75b3a90befaf729aedd1c67a523edfdf9ad4a8bc
Marcos Alexandre <marcos.alexandre34@gmail.com>
2020-07-02 23:59:03 -0300

    ajustes postgres

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
-
+/*
 const api = axios.create({
     baseURL: 'https://be-the-hero-sga.herokuapp.com',    
+});
+*/
+const api = axios.create({
+    baseURL: 'https://betheherosga.herokuapp.com/',    
 });
 
 export default api;
